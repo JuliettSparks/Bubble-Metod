7a8334245a70e6969f63cc30467ee15c667cd0cb
Juliett Sparks Jones <32524167+JuliettSparks@users.noreply.github.com>
2017-10-11 13:36:13 -0500

    Update Ordenar.cpp
    Bubble Metod Higher to Lower and Lower to Higher
    Thirty Elements in a Vector

--- Ordenar.cpp
+++ Ordenar.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main(){
 	int vector[10],aux1,aux2;
-	for (int i=0; i<10; i++){
+	for (int i=0; i<30; i++){
 		cout<<"Dame el "<<i+1<<" n"<<char(163)<<"mero\n";
 		cin>>vector[i];
 	}
-	for(int i=0; i<10; i++){
+	for(int i=0; i<30; i++){
 		aux1=i;
-		for(int j= i+1; j<10; j++)
+		for(int j= i+1; j<30; j++)
 		if(vector[aux1]< vector[j])
 		aux1=j;
 		aux2=vector[aux1];
@@ -16,12 +16,12 @@ int main(){
 		vector[i]=aux2;
 	}
 	cout<<"Mayor a Menor\n";
-	for(int i=0; i<10; i++){
+	for(int i=0; i=30; i++){
 		cout<<"["<<vector[i]<<"]\n";
 	}
-		for(int i=0; i<10; i++){
+		for(int i=0; i<30; i++){
 		aux1=i;
-		for(int j= i+1; j<10; j++)
+		for(int j= i+1; j<30; j++)
 		if(vector[aux1]> vector[j])
 		aux1=j;
 		aux2=vector[aux1];
@@ -29,7 +29,7 @@ int main(){
 		vector[i]=aux2;
 	}
 		cout<<"Menor a Mayor\n";
-		for(int i=0; i<10; i++){
+		for(int i=0; i<30; i++){
 		cout<<"["<<vector[i]<<"]\n";
 	}
 }
